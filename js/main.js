0a393972f6e3ff29c87595cf70525519c2ad0fd1
bakenray <lpqstrong@163.com>
2018-08-26 18:34:34 +0800

    add js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
+!function(){
+	var varMove = document.querySelectorAll('.nav-move > li')
+	var caseLists = document.querySelectorAll('.case-content > div.case-list')
+	for(let i =0; i<varMove.length;i++){
+		varMove[i].onclick=function(e){
+			varMove[i].classList.add('nav-border')
+			getEleSiblings(varMove[i],'nav-border')
+			console.log(varMove[i])
+			caseLists[i].classList.add('on')
+			getEleSiblings(caseLists[i],'on')
+			console.log(caseLists[i])
+		}
+	}	
+	// 获取兄弟元素并去除类
+	function getEleSiblings(ele,action){  
+		var eleParent = ele.parentNode.children
+		for(let i=0;i<eleParent.length;i++){
+			if(eleParent[i].nodeType ===1 && eleParent[i]!= ele){
+				eleParent[i].classList.remove(action)
+			}
+		}
+	}
 
+}.call()
 
